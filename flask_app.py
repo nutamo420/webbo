88a8d73d06865a3824c13c6e294974a729b8d0ec
Nutkrita Amornsupakit <64011495@kmitl.ac.th>
2021-11-18 17:15:01 +0700

    eee

--- flask_app.py
+++ flask_app.py
@@ -1,19 +1,21 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello Ajarn Sun!'
+def homepage():
+    return render_template('index.html')
 
-@app.route('/lala')
-def lala():
-    return 'Lala'
+@app.route('/currency')
+def currency_form():
+    return render_template('form.html')
 
-@app.route('/lala/lovesong')
-def lalalovesong():
-    return 'Lalalovesong'
-
+@app.route('/result', methods = ['GET'])
+def currency_result():
+    result = requests.get("http://data.fixer.io/api/latest?access_key=88e986137779229d0ead7334b6a91252")
+    jsondata = result.json()
+    eur_thb = jsondata['rates']['THB']
+    eur = request.args.get('amount')
+    thb = float(eur)*float(eur_thb)
+    return render_template('result.html',amount_eur=eur,amount_thb=thb)
